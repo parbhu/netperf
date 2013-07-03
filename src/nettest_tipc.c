65a9ed2f9ef6e1436c8a82bae44836bf3e91b31b
karin nilsson <karin.nilsson@ericsson.com>
2013-07-03 11:21:58 +0200

    tipc support: Initiating tipc structs and receiving request from netperf.
    New file nettest_tipc.h initiating tipc request, response and
    result structs.
    Change nettest_tipc.c to request TIPC_STREAM test case from
    netserver.
    
    Signed-off-by: karin nilsson <karin.nilsson@ericsson.com>

--- src/nettest_tipc.c
+++ src/nettest_tipc.c
@@ -8,17 +8,40 @@
 /*                        */
 /**************************/
 
+#include <stdio.h>  		// printf
+#include "netlib.h" 		// netperf_request_struct
+#include "netsh.h"  	 	// debug
+#include "nettest_tipc.h"	// tipc_XXX_XXX_struct
+
 void 
 send_tipc_stream(char remote_host[]) 
 {
   printf("netperf: TIPC stream test.\n");
+
+  struct tipc_stream_request_struct 	*tipc_stream_request;
+  struct tipc_stream_response_struct 	*tipc_stream_response;
+  struct tipc_stream_results_struct 	*tipc_stream_results;
+
+  netperf_request.content.request_type = DO_TIPC_STREAM;
+  if (debug > 1) {
+        fprintf(where,
+                "netperf: send_tcp_stream: requesting TCP stream test\n");
+  }
+
+  send_request();
 }
+
+
 
 void
 recv_tipc_stream()
 {
-  printf("netserver: TIPC stream test.\n");
+  FILE *fp;
+  fp = fopen("netserver_output","a");
+  fprintf(fp, "netserver: TIPC stream test.\n");
 }
+
+
 
 void 
 send_tipc_rr(char remote_host[])
@@ -29,7 +52,7 @@ send_tipc_rr(char remote_host[])
 void
 recv_tipc_rr()
 {
-  printf("netserver: TIPC stream test.\n");
+  printf("netserver: TIPC rr test.\n");
 }
 
 
---
+++ src/nettest_tipc.h
@@ -0,0 +1,20 @@
+/* Test specific definitions for netperf's tipc socket tests. */
+
+struct tipc_stream_request_struct {
+};
+
+struct tipc_stream_response_struct {
+};
+
+struct tipc_stream_result_struct {
+};
+
+struct tipc_rr_request_struct {
+};
+
+struct tipc_rr_response_struct {
+};
+
+struct tipc_rr_result_struct {
+};
+
